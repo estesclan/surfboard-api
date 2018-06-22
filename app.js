371fb02d8ce1cc06cbe8824f096df62897c3b560
Peter Estes <peternigelestes@gmail.com>
2018-06-22 16:07:59 -0400

    up to paginate

--- app.js
+++ app.js
@@ -1,68 +1,117 @@
-require('dotenv').config()
-const app = require('express')()
+require("dotenv").config()
+const app = require("express")()
 const port = process.env.PORT || 5001
-const bodyParser = require('body-parser')
-const NodeHTTPError = require('node-http-error')
-const { propOr, isEmpty, compose, not, join } = require('ramda')
-const reqFieldChecker = require('./lib/required-field-check')
-const { addBoard } = require('./dal')
+const bodyParser = require("body-parser")
+const NodeHTTPError = require("node-http-error")
+const { pathOr, propOr, isEmpty, compose, not, join } = require("ramda")
+const reqFieldChecker = require("./lib/required-field-check")
+const {
+	addBoard,
+	getBoard,
+	listBoards,
+	updateBoard,
+	deleteBoard
+} = require("./dal")
 
 app.use(bodyParser.json())
 
-app.get('/', function(req, res, next) {
-  res.send('Welcome to the brocean, brah.  Gnar Gnar.')
+app.get("/", function(req, res, next) {
+	res.send("Welcome to the brocean, brah.  Gnar Gnar.")
 })
 
-app.post('/boards', (req, res, next) => {
-  const newBoard = propOr({}, 'body', req)
+app.get("/boards/:sku", function(req, res, next) {
+	const boardSku = `board_${req.params.sku}`
+	console.log(boardSku)
+	//getBoard(boardSku, x)
+	//db.get(boardSku, instructions) instead of getBoard
+	getBoard(boardSku, function(err, board) {
+		if (err) {
+			next(new NodeHTTPError(err.status, err.message, err))
+			return
+		}
+		res.status(200).send(board)
+	})
+})
 
-  if (isEmpty(newBoard)) {
-    next(
-      new NodeHTTPError(
-        400,
-        'Brah, add a board to the request body.  Ensure the Content-Type is application/json. Dude!'
-      )
-    )
-  }
+app.get("/boards", function(req, res, next) {
+	const limit = Number(pathOr(10, ["query", "limit"], req))
+	console.log("limit", limit)
+	const paginate = pathOr(null, ["query", "start_key"], req)
+	console.log("paginate", paginate)
+	listBoards(limit, paginate, function(err, boards) {
+		if (err) {
+			next(new NodeHTTPError(err.status, err.message, err))
+			return
+		}
+		res.status(200).send(boards)
+	})
+})
 
-  const missingFields = reqFieldChecker(
-    ['name', 'category', 'price', 'sku'],
-    newBoard
-  )
+app.post("/boards", (req, res, next) => {
+	const newBoard = propOr({}, "body", req)
 
-  const sendMissingFieldError = compose(not, isEmpty)(missingFields)
+	if (isEmpty(newBoard)) {
+		next(
+			new NodeHTTPError(
+				400,
+				"Brah, add a board to the request body.  Ensure the Content-Type is application/json. Dude!"
+			)
+		)
+	}
 
-  if (sendMissingFieldError) {
-    next(
-      new NodeHTTPError(
-        400,
-        `Brah, you didnt pass all the required fields: ${join(
-          ', ',
-          missingFields
-        )}`,
-        { josh: 'is Cool and humble', jp: 'is Cool' }
-      )
-    )
-  }
+	const missingFields = reqFieldChecker(
+		["name", "category", "price", "sku"],
+		newBoard
+	)
 
-  addBoard(newBoard, function(err, result) {
-    if (err)
-      next(
-        new NodeHTTPError(err.status, err.message, { ...err, max: 'isCool' })
-      )
-    res.status(201).send(result)
-  })
+	const sendMissingFieldError = compose(not, isEmpty)(missingFields)
+
+	if (sendMissingFieldError) {
+		next(
+			new NodeHTTPError(
+				400,
+				`Brah, you didnt pass all the required fields: ${join(
+					", ",
+					missingFields
+				)}`,
+				{ josh: "is Cool and humble", jp: "is Cool" }
+			)
+		)
+	}
+
+	addBoard(newBoard, function(err, result) {
+		if (err)
+			next(
+				new NodeHTTPError(err.status, err.message, { ...err, max: "isCool" })
+			)
+		res.status(201).send(result)
+	})
+})
+
+app.put("/boards/:sku", (req, res, next) => {
+	const updatedBoard = propOr({}, "body", req)
+	const missingFields = reqFieldChecker(
+		["name", "category", "price", "sku"],
+		updatedBoard
+	)
+	if (not(isEmpty(missingFields))) {
+		next(new NodeHTTPError(err.status, err.message, err))
+		return
+	}
+	updateBoard(updatedBoard, function(err, result) {
+		res.status(201).send(result)
+	})
 })
 
 app.use((err, req, res, next) => {
-  console.log(
-    `WIPEOUT! \n\nMETHOD ${req.method} \nPATH ${req.path}\n${JSON.stringify(
-      err,
-      null,
-      2
-    )}`
-  )
-  res.status(err.status || 500).send(err)
+	console.log(
+		`WIPEOUT! \n\nMETHOD ${req.method} \nPATH ${req.path}\n${JSON.stringify(
+			err,
+			null,
+			2
+		)}`
+	)
+	res.status(err.status || 500).send(err)
 })
 
-app.listen(port, () => console.log('Brah!', port))
+app.listen(port, () => console.log("Brah!", port))
--- dal.js
+++ dal.js
@@ -1,20 +1,37 @@
-require('dotenv').config()
-const PouchDB = require('pouchdb-core')
-PouchDB.plugin(require('pouchdb-adapter-http'))
+require("dotenv").config()
+const PouchDB = require("pouchdb-core")
+PouchDB.plugin(require("pouchdb-adapter-http"))
 
-const { merge, prop } = require('ramda')
-const pkGen = require('./lib/pk-gen')
+const { merge, prop, map } = require("ramda")
+const pkGen = require("./lib/pk-gen")
 
 const db = new PouchDB(
-  `${process.env.COUCH_HOSTNAME}${process.env.COUCH_DBNAME}`
+	`${process.env.COUCH_HOSTNAME}${process.env.COUCH_DBNAME}`
 )
 
+const getBoard = (boardSku, instructions) => db.get(boardSku, instructions)
+
+const listBoards = (limit, paginate, callback) =>
+	db.allDocs(
+		paginate
+			? { include_docs: true, limit, start_key: `${paginate}\ufff0` }
+			: { include_docs: true, limit },
+		function(err, result) {
+			if (err) callback(err)
+			callback(null, map(row => row.doc, result.rows))
+		}
+	)
+
 const addBoard = (board, cb) => {
-  const modifiedBoard = merge(board, {
-    type: 'board',
-    _id: pkGen('board_', '-', prop('sku', board))
-  })
-  db.put(modifiedBoard, cb)
+	const modifiedBoard = merge(board, {
+		type: "board",
+		_id: pkGen("board_", "-", prop("sku", board))
+	})
+	db.put(modifiedBoard, cb)
 }
 
-module.exports = { addBoard }
+const updateBoard = (board, instructions) => db.put(board, instructions)
+
+const deleteBoard = (board, instructions) => db.remove(board, instructions)
+
+module.exports = { addBoard, getBoard, listBoards, updateBoard, deleteBoard }
